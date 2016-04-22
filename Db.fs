35506ec811666cc89395a42160041e1cf0eedcdb
Olov Johansson <johansson.olov@gmail.com>
2016-04-22 16:38:25 +0200

    Initial Commit
    Initial Commit

---
+++ Db.fs
@@ -0,0 +1,63 @@
+﻿namespace TasksApi.Db
+
+open System.Collections.Generic
+
+type Team = Frontend | Backend | Test
+type Stage = Utveckling | Test | Merge
+type Task = {Id:int; AssignedTeam:Team; Story:string; Description:string; InStage:Stage; NeedsRebase:bool; DependsOn:Task list}
+
+module Db = 
+    let private tasksStorage = new Dictionary<int, Task>()
+    tasksStorage.Add(0,{Id = 0;AssignedTeam = Frontend; Story = "SARA-1789"; Description = "En kort beskrivning"; InStage = Utveckling; NeedsRebase = false; DependsOn = []})
+    
+
+    let getTasks () = 
+        tasksStorage.Values |> Seq.map(fun t -> t)
+
+    let createTask task =
+        let id = tasksStorage.Values.Count + 1
+        let newTask = {
+            Id = id
+            AssignedTeam = task.AssignedTeam
+            Story = task.Story
+            Description = task.Description
+            InStage = task.InStage
+            NeedsRebase = task.NeedsRebase
+            DependsOn = task.DependsOn
+        }
+        tasksStorage.Add(id, newTask)
+        newTask
+
+    let updateTaskById taskId replacementTask =
+        if tasksStorage.ContainsKey(taskId) then
+            let updatedTask = {
+                Id = taskId
+                AssignedTeam = replacementTask.AssignedTeam
+                Story = replacementTask.Story
+                Description = replacementTask.Description
+                InStage = replacementTask.InStage
+                NeedsRebase = replacementTask.NeedsRebase
+                DependsOn = replacementTask.DependsOn
+            }
+            tasksStorage.[taskId] <- updatedTask
+            Some updatedTask
+        else
+            None
+
+    let updateTask replacementTask = 
+        updateTaskById replacementTask.Id replacementTask
+
+    let deleteTask taskId =
+        tasksStorage.Remove(taskId) |> ignore
+
+    let getTask taskId = 
+        if tasksStorage.ContainsKey(taskId) then
+            Some tasksStorage.[taskId]
+        else
+            None
+    
+    let isTaskExists = tasksStorage.ContainsKey
+
+ 
+
+
---
+++ Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Suave.Web
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.WebPart
+open Suave.Utils.Choice
+open TasksApi.Db
+open TasksApi.Rest
+
+
+[<EntryPoint>]
+let main _ =    
+    let tasksWebPart = rest "tasks" {
+        GetAll = Db.getTasks
+        Create = Db.createTask
+        Update = Db.updateTask
+        Delete = Db.deleteTask        
+        GetById = Db.getTask
+        UpdateById = Db.updateTaskById
+        IsExists = Db.isTaskExists
+    }
+
+    startWebServer defaultConfig tasksWebPart
+    0 // return an integer exit code
+
+
+
